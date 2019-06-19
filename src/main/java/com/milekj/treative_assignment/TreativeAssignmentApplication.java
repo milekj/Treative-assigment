cb90dcc701dd4df401933c26ff8a8e22f7437e7a
mjanowsk <m.i.l.e.k.j.a.n@gmail.com>
2019-06-19 15:56:05 +0200

    Initial commit

---
+++ src/main/java/com/milekj/treative_assignment/TreativeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.milekj.treative_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreativeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreativeAssignmentApplication.class, args);
+	}
+
+}
